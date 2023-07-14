a138150f6c845e3c39e67c49e700839e0b5496e5
saciinol <darrenicolas18@gmail.com>
2023-07-14 15:30:14 +0800

    sdada

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,14 @@
 module.exports = {
 	content: ['./dist/**/*.{html,js}'],
 	theme: {
-		colors: {
-			'bg-1': '#2c2c32',
-			'bg-2': '#ffffff',
-			'bg-3': '#f5f5f5',
-		},
 		extend: {
 			fontFamily: {
 				'body': ['Open Sans'],
+			},
+			colors: {
+				'bg-1': '#2c2c32',
+				'bg-2': '#ffffff',
+				'bg-3': '#f5f5f5',
 			},
 		},
 	},
