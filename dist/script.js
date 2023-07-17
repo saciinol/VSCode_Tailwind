cfd94486233f50af19827500f17b2c50bbfe38c1
saciinol <darrenicolas18@gmail.com>
2023-07-17 15:11:14 +0800

    responsive

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,17 @@
-const toggle = document.querySelector('.toggle-menu');
-const headNav = document.querySelector('.header__nav');
+const toggle = document.querySelector('.toggle-menu')
+const headNav = document.querySelector('.header__nav')
 
 toggle.addEventListener('click', () => {
-	headNav.classList.toggle('open');
-});
+	headNav.classList.toggle('open')
+})
+
+var slider = tns({
+	container: '.reviews__slider',
+	items: 1,
+	slideBy: 'page',
+	autoplay: false,
+	controlsText: [
+		'<i class="fa-solid fa-chevron-left">', 
+		'<i class="fa-solid fa-chevron-right">',
+	],
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,26 @@
 module.exports = {
 	content: ['./dist/**/*.{html,js}'],
 	theme: {
+		screens: {
+			'sm': '640px',
+			// => @media (min-width: 640px) { ... }
+	  
+			'md': '768px',
+			// => @media (min-width: 768px) { ... }
+	  
+			'lg': '1024px',
+			// => @media (min-width: 1024px) { ... }
+	  
+			'xl': '1200px',
+			// => @media (min-width: 1200px) { ... }
+	  
+			'2xl': '1536px',
+			// => @media (min-width: 1536px) { ... }
+		  },
+	  
 		extend: {
 			fontFamily: {
-				'body': ['Open Sans'],
+				body: ['Open Sans'],
 			},
 			colors: {
 				'bg-1': '#2c2c32',
@@ -14,4 +31,4 @@ module.exports = {
 		},
 	},
 	plugins: [],
-};
+}
