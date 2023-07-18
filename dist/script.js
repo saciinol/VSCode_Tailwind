a2dbfce86932ce3514456d2b1e1c77095f55761d
saciinol <darrenicolas18@gmail.com>
2023-07-18 09:28:38 +0800

    finished

--- dist/script.js
+++ dist/script.js
@@ -10,8 +10,7 @@ var slider = tns({
 	items: 1,
 	slideBy: 'page',
 	autoplay: false,
-	controlsText: [
-		'<i class="fa-solid fa-chevron-left">', 
-		'<i class="fa-solid fa-chevron-right">',
-	],
+	controlsText: ['<i class="fa-solid fa-chevron-left"></i>', '<i class="fa-solid fa-chevron-right"></i>'],
+	nav: false,
+	mouseDrag: true,
 })
